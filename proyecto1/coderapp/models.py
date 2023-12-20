71617a002895a4aa461f9b5289b6ae1b35f31fa1
Danieljb93 <danieljuradobenavideswd@gmail.com>
2023-12-20 15:52:15 -0500

    tercera

--- proyecto1/coderapp/models.py
+++ proyecto1/coderapp/models.py
@@ -6,6 +6,35 @@ class Profesor(models.Model):
     apellido = models.CharField(max_length=30)
     email = models.EmailField()
 
+    class Meta:
+        verbose_name_plural = "Profesores"
+        ordering=["nombre"]
+
     def __str__(self):
         return f"{self.nombre}, {self.apellido}"
+    
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.nombre}, {self.apellido}"
+    
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=40)
+    camada = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.nombre} -- {self.camada}"
+
+
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=40)
+    fechaDeEntrega = models.DateField()
+
+    def __str__(self):
+        return f"{self.nombre} -- {self.fechaDeEntrega}"
+    
 
--- proyecto1/coderapp/urls.py
+++ proyecto1/coderapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import Leer_profesor, Leer_alumnos, index
+from .views import profesores, estudiantes, cursos, entregables, index   
 
 
 urlpatterns = [
     
-    path('profesore/', Leer_profesor),
-    path('alumnos/', Leer_alumnos),
-    path('', index)
+    path('profesores/', profesores, name='profesores'),
+    path('alumnos/', estudiantes, name='estudiantes'),
+    path('cursos/', cursos, name='cursos'),
+    path('entregables/', entregables, name='entregables'),
+    path('Inicio', index, name='index')
 ]
